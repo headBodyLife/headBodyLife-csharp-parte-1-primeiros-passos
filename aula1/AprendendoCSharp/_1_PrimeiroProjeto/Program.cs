03a639ae292d1109811762b2b35a48f369478fd5
headBodyLife <allan.bt167@gmail.com>
2022-01-19 11:26:37 -0300

    aula1

---
+++ aula1/AprendendoCSharp/_1_PrimeiroProjeto/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace _1_PrimeiroProjeto
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Nosso primeiro Projeto em Rider!");
+        }
+    }
+}
