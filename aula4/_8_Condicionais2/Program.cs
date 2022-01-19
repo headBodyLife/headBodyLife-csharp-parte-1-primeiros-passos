fee81faedeebf140cfee2e01fd1a361ec2fda6c3
headBodyLife <allan.bt167@gmail.com>
2022-01-19 16:59:32 -0300

    Condicionais com Booleanos

---
+++ aula4/_8_Condicionais2/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace _8_Condicionais2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            {
+                
+                Console.WriteLine("Executando projeto 7 - Condicionais");
+                int idadeJoao = 16;
+                //int quantidadePessoa = 2;
+                //bool acompanhado = quantidadePessoa >= 2;
+                bool acompanhado = true;
+                
+                // or || and && 
+                if (idadeJoao >= 18 && acompanhado == true)
+                {
+                    Console.WriteLine("Pode entrar");
+                }
+                else
+                {
+                    Console.WriteLine("Não pode entrar");
+                }
+            }
+        }
+    }
+}
