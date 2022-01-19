1c43b96c080f171c37726a118a82662289cd1db1
headBodyLife <allan.bt167@gmail.com>
2022-01-19 19:02:33 -0300

    Laço Encadeado

---
+++ aula5/_P12_CalculaInvestimentoAoLongoPrazo/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Runtime.InteropServices;
+
+namespace _P12_CalculaInvestimentoAoLongoPrazo
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            
+            Console.WriteLine("Executando o projeto 12");
+            double fatorRendimento = 1.0036;
+            double valorInvestido = 1000;
+            for (int contadorAno = 1; contadorAno <= 5; contadorAno++)
+            {
+                for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+                {
+                    valorInvestido *= fatorRendimento;
+                }
+
+                fatorRendimento += 0.0010;
+            }
+            Console.WriteLine("Ao terminoo do investimento, você terá R$ "+ valorInvestido);
+        }
+        
+    }
+}
