d0f20aeec92b5b2c85d1aa29cd63e4d157b5923d
headBodyLife <allan.bt167@gmail.com>
2022-01-19 18:51:43 -0300

    Laço de Repetição For

---
+++ aula5/_P11_CalculaPoupanca2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace _P11_CalculaPoupanca2
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 11");
+            double valorInvestido = 1000;
+            for (int contadorMes = 1; contadorMes <= 12; contadorMes++)
+            {
+                valorInvestido *= 1.0036;
+                Console.WriteLine("Apos o {0} mês(es), " +
+                                  "você terá {1}", contadorMes,valorInvestido);
+            }
+        }
+    }
+}
