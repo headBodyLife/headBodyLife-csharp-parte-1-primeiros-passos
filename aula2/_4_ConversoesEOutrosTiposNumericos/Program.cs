7e4417af42e2656dee50c26027ea4a37af6df835
headBodyLife <allan.bt167@gmail.com>
2022-01-19 14:56:45 -0300

    aula2

---
+++ aula2/_4_ConversoesEOutrosTiposNumericos/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+
+namespace _4_ConversoesEOutrosTiposNumericos
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Executando o projeto 4");
+            
+            double salario;
+            salario = 1200.50;
+            Console.WriteLine(salario);
+            
+            //int para numeros de 32 bits
+            //cast para converter
+            int salarioEmInteiro = (int)salario;
+            Console.WriteLine(salarioEmInteiro);
+            
+            //long para numeros de 64 bits
+            long idade= 1300000000000;
+            Console.WriteLine(idade);
+            
+            //short para numeros de 16 bits
+            short quantidadeProdutos = 15000;
+            Console.WriteLine(quantidadeProdutos);
+            
+            //float tem menor precisão
+            float altura = 1.80f;
+            Console.WriteLine(altura);
+            
+        }
+    }
+}
